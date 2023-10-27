3efb2a7b9b689a7dbeabafaef386f5092524eec3
kys <kang_ys_1@naver.com>
2023-10-27 22:04:51 +0900

    테이블 생성 쿼리문(해석 완료)

--- create_table/create_table.sql
+++ create_table/create_table.sql
@@ -3,7 +3,12 @@ CREATE TABLE address (
 	zip_code		  VARCHAR(6),
 	address_detail	  VARCHAR(20));
 	
-ALTER TABLE address ADD CONSTRAINT pk_zip_code PRIMARY KEY (zip_code);
+-- 기본키 지정
+-- CONSTRAINT PK_이름 PRIMARY KEY (적용 컬럼)
+-- PK_이름으로 PK를 생성합니다.
+ALTER TABLE address 
+ADD 
+CONSTRAINT pk_zip_code PRIMARY KEY (zip_code);
 
 
 -- customer 테이블 생성
@@ -19,28 +24,32 @@ CREATE TABLE customer (
 	zip_code		  VARCHAR(6));
 
 ALTER TABLE customer ADD CONSTRAINT pk_customer PRIMARY KEY (customer_id);
+-- customer 테이블의 zip_code를 customer_zip_code이름으로 FOREIGN KEY로 지정하고, 이는 address 테이블의 (zip_code)를 참조한다.
 ALTER TABLE customer ADD CONSTRAINT fk_customer_zip_code FOREIGN KEY (zip_code) REFERENCES address (zip_code);
 
 
 -- item 테이블 생성
+-- NUMBER(10,0) -> INT
+-- NUMBER(10,0): 최대 정수 10자리, 소수 0자리 숫자형 데이터
 CREATE TABLE item ( 
 	item_id			VARCHAR(10),
 	product_name	VARCHAR(30),
 	product_desc	VARCHAR(50),
 	category_id		VARCHAR(10),
-	price			NUMBER(10,0));
+	price			INT);
 	
 ALTER TABLE item ADD CONSTRAINT pk_item PRIMARY KEY (item_id);
 
 
 -- reservation 테이블 생성
+-- ??? CONSTRAINT nn_reservation_customer_id NOT NULL enable -> NOT NULL
 CREATE TABLE reservation ( 
 	reserv_no	    VARCHAR(30),
 	reserv_date	    VARCHAR(8),
 	reserv_time	    VARCHAR(4),
-	customer_id	    VARCHAR(10) CONSTRAINT nn_reservation_customer_id NOT NULL enable,
+	customer_id	    VARCHAR(10) NOT NULL,
 	branch		    VARCHAR(20),
-	visitor_cnt	    NUMBER(3,0),
+	visitor_cnt	    TINYINT,
     cancel 		    VARCHAR(1));
 
 ALTER TABLE reservation ADD CONSTRAINT pk_reservation PRIMARY KEY (reserv_no);
@@ -48,12 +57,14 @@ ALTER TABLE reservation ADD CONSTRAINT fk_reservation_customer_id FOREIGN KEY (c
 
 
 -- order_info 테이블 생성
+-- NUMBER(3, 0) -> TINYINT
+-- NUMBER(3, 0): 최대 정수 3자리, 소수 0자리 숫자형 데이터
 CREATE TABLE order_info ( 
 	order_no	    VARCHAR(30),
 	item_id		    VARCHAR(10),
 	reserv_no	    VARCHAR(30),
-	quantity	    NUMBER(3,0),
-    sales           NUMBER(10,0));
+	quantity	    TINYINT,
+    sales           INT);
 
 ALTER TABLE order_info ADD CONSTRAINT pk_order_info PRIMARY KEY (order_no, item_id);
 ALTER TABLE order_info ADD CONSTRAINT fk_order_info_item_id FOREIGN KEY (item_id) REFERENCES item (item_id);
--- readme.md
+++ readme.md
@@ -1,12 +1,19 @@
 # 모두의 SQL 실습 클론코딩
 
-## 프로젝트 개요
+## [프로젝트 개요]
 
 1. 실습에서 제공하는 예약 데이터와 매출 데이터를 토대로 데이터 분석
 2. oracle 문법을 mariadb 문법으로 바꾸기
 3. 기본 SQL문을 학습하고, 데이터 분석 실습을 통해 실무에서 SQL 활용을 학습.
 
-## 프로젝트 순서
 
+
+
+## [배운점]
 ### 1. 테이블 생성
-* 
+* CONSTRAINT PK_이름 PRIMARY KEY (적용 컬럼): PK_이름으로 PK를 생성합니다.
+* ALTER TABLE customer ADD CONSTRAINT fk_customer_zip_code FOREIGN KEY (zip_code) REFERENCES address (zip_code); : customer 테이블의 zip_code를 customer_zip_code이름으로 FOREIGN KEY로 지정하고, 이는 address 테이블의 (zip_code)를 참조한다.
+
+## [궁금증]
+### 1. 테이블 생성
+* CONSTRAINT nn_reservation_customer_id NOT NULL enable -> NOT NULL
